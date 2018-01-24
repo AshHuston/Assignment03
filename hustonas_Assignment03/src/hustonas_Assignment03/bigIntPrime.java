6e05badd07cab01848e617ce5c95ca4302562e93
Ash <Ash@Stella.Home>
2018-01-24 08:56:15 -0500

    Method stub for Assignment03.

---
+++ hustonas_Assignment03/src/hustonas_Assignment03/bigIntPrime.java
@@ -0,0 +1,18 @@
+/***************************************
+ * Name: Ash Huston
+ * Assignment #: Assignment01
+ * Course #, Section: Course 2045 Section 01
+ * Due Date: 01/30/2018
+ * Description: This method is used to determine if a number of type BigInteger is prime or not.
+ * 
+ */
+package hustonas_Assignment03;
+
+import java.math.BigInteger;
+
+public class bigIntPrime {
+	//Method stub.
+	public static boolean isPrime(BigInteger num){
+		return true;
+	}
+}
